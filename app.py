e94081f6fd250f75736d70ce0cbb1a2776aa1360
129fc5e32c631b5007cb2cd8d45d416002b42bab <16278483+DarkCeptor44@users.noreply.github.com>
2020-06-03 02:24:31 -0300

    PEP8 Minor Improvements

--- app.py
+++ app.py
@@ -7,27 +7,27 @@ MAX_PIXEL_VALUE = 255
 
 def get_pixel_matrix(img):
     img.thumbnail((100, 100))
-    pixels = list(img.getdata())
-    return [pixels[i:i+img.width] for i in range(0, len(pixels), img.width)]
+    p = list(img.getdata())
+    return [p[i:i+img.width] for i in range(0, len(p), img.width)]
 
 
 def get_intensity_matrix(pixels_matrix):
-    intensity_matrix = []
+    im = []
     for row in pixels_matrix:
         intensity_row = []
         for p in row:
             intensity = (p[0] + p[1] + p[2] / 3.0)
             intensity_row.append(intensity)
-        intensity_matrix.append(intensity_row)
+        im.append(intensity_row)
 
-    return intensity_matrix
+    return im
 
 
-def normalize_intensity_matrix(intensity_matrix):
+def normalize_intensity_matrix(im):
     normalized_intensity_matrix = []
-    max_pixel = max(map(max, intensity_matrix))
-    min_pixel = min(map(min, intensity_matrix))
-    for row in intensity_matrix:
+    max_pixel = max(map(max, im))
+    min_pixel = min(map(min, im))
+    for row in im:
         rescaled_row = []
         for p in row:
             r = MAX_PIXEL_VALUE * (p - min_pixel) / float(max_pixel - min_pixel)
@@ -37,27 +37,27 @@ def normalize_intensity_matrix(intensity_matrix):
     return normalized_intensity_matrix
 
 
-def convert_to_ascii(intensity_matrix, ascii_chars):
-    ascii_matrix = []
-    for row in intensity_matrix:
+def convert_to_ascii(im, ascii_chars):
+    am = []
+    for row in im:
         ascii_row = []
         for p in row:
             ascii_row.append(ascii_chars[int(p/MAX_PIXEL_VALUE * len(ascii_chars)) - 1])
-        ascii_matrix.append(ascii_row)
+        am.append(ascii_row)
 
-    return ascii_matrix
+    return am
 
 
-def print_ascii_matrix(ascii_matrix):
-    for row in ascii_matrix:
+def print_ascii_matrix(am):
+    for row in am:
         line = [p for p in row]
         print("".join(line))
 
 
 filepath = sys.argv[1]
 
-img = Image.open(filepath)
-pixels = get_pixel_matrix(img)
+image = Image.open(filepath)
+pixels = get_pixel_matrix(image)
 
 intensity_matrix = get_intensity_matrix(pixels)
 intensity_matrix = normalize_intensity_matrix(intensity_matrix)
